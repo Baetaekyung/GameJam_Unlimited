58a99ad531de6a79baefb7a02a1198d775a726bf
gksdnfgksdnf <kkho8489@gmail.com>
2024-12-18 12:11:51 +0900

    Sliding

--- Assets/01Scripts/Obstacle/Platform/SlidingPlatform.cs
+++ Assets/01Scripts/Obstacle/Platform/SlidingPlatform.cs
@@ -4,17 +4,39 @@ using UnityEngine;
 
 public class SlidingPlatform : MonoBehaviour
 {
+    [SerializeField] private PhysicsMaterial2D _defaultMat;
+    [SerializeField] private PhysicsMaterial2D _slidingMat;
+
+    [SerializeField] private BallController _player;
+
+    private Rigidbody2D _rbCompo;
+    private CircleCollider2D _collider;
+
+    private void Awake()
+    {
+        _rbCompo = _player.GetComponent<Rigidbody2D>();
+        _collider = _player.GetComponent<CircleCollider2D>();
+    }
+
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-            Debug.Log("µé¾î¿È");
+        {
+            _rbCompo.sharedMaterial = _slidingMat;
+            _collider.sharedMaterial = _slidingMat;
+        }
+            
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-            Debug.Log("³ª°£");
+        {
+            _rbCompo.sharedMaterial = _defaultMat;
+            _collider.sharedMaterial = _defaultMat;
+        }
+            
     }
 }

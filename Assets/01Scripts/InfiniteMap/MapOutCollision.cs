145859ea20a4a6362aeee4724934163b8f2f66ae
Baetaekyung <tkwayi2129@gmail.com>
2024-12-20 00:09:01 +0900

    챌린지 씬 완성

--- Assets/01Scripts/InfiniteMap/MapOutCollision.cs
+++ Assets/01Scripts/InfiniteMap/MapOutCollision.cs
@@ -5,31 +5,30 @@ using UnityEngine;
 
 public class MapOutCollision : MonoBehaviour
 {
-    private bool _isTriggered = false;
-    
+    private Collider2D _col;
+
+    private void Awake()
+    {
+        _col = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (_isTriggered) return;
-        
         if (other.gameObject.TryGetComponent(out BallController player))
         {
             if (player is not null)
             {
-                InfiniteMapManager.Instance.UpHeight();
-                InfiniteMapManager.Instance.CreateMap();
+                InfiniteMapManager.Instance?.UpHeight();
+                InfiniteMapManager.Instance?.CreateMap();
 
                 if (SaveManager.Exist("maxScore.json"))
                 {
                     MaxScore savedMaxScore = SaveManager.Load<MaxScore>("maxScore.json");
 
-                    if (savedMaxScore.maxScore < InfiniteMapManager.Instance.height)
+                    if (savedMaxScore.maxScore < InfiniteMapManager.Instance?.height)
                     {
                         InfiniteMapManager.Instance.maxScore.maxScore = InfiniteMapManager.Instance.height;
                         SaveManager.Save(InfiniteMapManager.Instance.maxScore, "maxScore.json");
-                    }
-                    else
-                    {
-                        return;
                     }
                 }
                 else
@@ -38,8 +37,9 @@ public class MapOutCollision : MonoBehaviour
                     InfiniteMapManager.Instance.maxScore.maxScore = InfiniteMapManager.Instance.height;
                     SaveManager.Save(InfiniteMapManager.Instance.maxScore, "maxScore.json");
                 }
-                _isTriggered = true;
             }
+
+            _col.enabled = false;
         }
     }
 }
